545bf741fb4a9e47899a0bdc4d80d81f67572280
Mahede Hasan <mahedehasan.cd91@gmail.com>
2024-01-22 01:10:15 +0600

    add history navLink

--- js/activePage.js
+++ js/activePage.js
@@ -1,35 +1,58 @@
 
 // side nav route
-const clickMyListing=()=>{
+const clickMyListing = () => {
     document.querySelector('#my-listing-page').style.display = 'block'
     document.querySelector('#my-profile-page').style.display = 'none'
     document.querySelector('#my-favorite-page').style.display = 'none'
     document.querySelector('#my-announcement-page').style.display = 'none'
 
+    // my-history-page selector added-second
+    document.querySelector('#my-history-page').style.display = 'none'
 }
-const clickMyProfile=()=>{
+const clickMyProfile = () => {
     document.querySelector('#my-listing-page').style.display = 'none'
     document.querySelector('#my-favorite-page').style.display = 'none'
     document.querySelector('#my-announcement-page').style.display = 'none'
+
+    // my-history-page selector added-second
+    document.querySelector('#my-history-page').style.display = 'none'
+
     document.querySelector('#my-profile-page').style.display = 'block'
 }
-const clickMyFavorite=()=>{
+const clickMyFavorite = () => {
     document.querySelector('#my-listing-page').style.display = 'none'
     document.querySelector('#my-profile-page').style.display = 'none'
     document.querySelector('#my-announcement-page').style.display = 'none'
+
+    // my-history-page selector added-second
+    document.querySelector('#my-history-page').style.display = 'none'
+
     document.querySelector('#my-favorite-page').style.display = 'block'
 }
-const clickMyAnnouncement=()=>{
+const clickMyAnnouncement = () => {
     document.querySelector('#my-listing-page').style.display = 'none'
     document.querySelector('#my-profile-page').style.display = 'none'
     document.querySelector('#my-favorite-page').style.display = 'none'
+
+    // my-history-page selector added-second
+    document.querySelector('#my-history-page').style.display = 'none'
+
     document.querySelector('#my-announcement-page').style.display = 'block'
+}
+
+// clickMyHistory event added-second
+const clickMyHistory = () => {
+    document.querySelector('#my-listing-page').style.display = 'none'
+    document.querySelector('#my-profile-page').style.display = 'none'
+    document.querySelector('#my-favorite-page').style.display = 'none'
+    document.querySelector('#my-announcement-page').style.display = 'none'
+    document.querySelector('#my-history-page').style.display = 'block'
 }
 
 // side nav active route
 const sideNavLinks = document.querySelectorAll('.nav-links');
-sideNavLinks.forEach(sideNavLink=>{
-    sideNavLink.addEventListener('click', ()=>{
+sideNavLinks.forEach(sideNavLink => {
+    sideNavLink.addEventListener('click', () => {
         document.querySelector('.active-route')?.classList.remove('active-route');
         sideNavLink.classList.add('active-route')
     })
@@ -38,36 +61,36 @@ sideNavLinks.forEach(sideNavLink=>{
 
 // My Listing active route
 const navLinkEls = document.querySelectorAll('.nav-link');
-navLinkEls.forEach(navLinkEl=>{
-    navLinkEl.addEventListener('click', ()=>{
+navLinkEls.forEach(navLinkEl => {
+    navLinkEl.addEventListener('click', () => {
         document.querySelector('.active')?.classList.remove('active');
         navLinkEl.classList.add('active');
     })
 })
 
 // my listing nav route
-const clickAllListing=()=>{
+const clickAllListing = () => {
     document.querySelector('.all-listing').style.display = 'block'
     document.querySelector('.publish').style.display = 'none'
     document.querySelector('.pending').style.display = 'none'
     document.querySelector('.expired').style.display = 'none'
 }
 
-const clickPublish=()=>{
+const clickPublish = () => {
     document.querySelector('.all-listing').style.display = 'none'
     document.querySelector('.pending').style.display = 'none'
     document.querySelector('.expired').style.display = 'none'
     document.querySelector('.publish').style.display = 'block'
 
 }
-const clickPending=()=>{
+const clickPending = () => {
     document.querySelector('.all-listing').style.display = 'none'
     document.querySelector('.expired').style.display = 'none'
     document.querySelector('.publish').style.display = 'none'
     document.querySelector('.pending').style.display = 'block'
 
 }
-const clickExpired=()=>{
+const clickExpired = () => {
     document.querySelector('.all-listing').style.display = 'none'
     document.querySelector('.pending').style.display = 'none'
     document.querySelector('.publish').style.display = 'none'
